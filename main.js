f479365be82353715a149c0078db0603051bd65e
Nia Laing <nialaing@Nias-MacBook-Air.local>
2020-07-16 01:46:37 -0400

    marsha's philanthropy page

---
+++ main.js
@@ -0,0 +1,54 @@
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e.target.className);
+    document.getElementById('my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    ul.lastElementChild.innerHTML = '<h4>Changed</h4>';
+  });
+  
+  
+  const nameInput = document.querySelector('#name');
+  nameInput.addEventListener('input', e => {
+    document.querySelector('.container').append(nameInput.value);
+  });
+  
+  
+  
+  const myForm = document.querySelector('#my-form');
+  const nameInput = document.querySelector('#name');
+  const emailInput = document.querySelector('#email');
+  const msg = document.querySelector('.msg');
+  const userList = document.querySelector('#users');
+  
+  
+  myForm.addEventListener('submit', onSubmit);
+  
+  function onSubmit(e) {
+    e.preventDefault();
+    
+    if(nameInput.value === '' || emailInput.value === '') {
+      // alert('Please enter all fields');
+      msg.classList.add('error');
+      msg.innerHTML = 'Please enter all fields';
+  
+      // Remove error after 3 seconds
+      setTimeout(() => msg.remove(), 3000);
+    } else {
+      // Create new list item with user
+      const li = document.createElement('li');
+  
+      // Add text node with input values
+      li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+  
+      // Add HTML
+      // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+  
+      // Append to ul
+      userList.appendChild(li);
+  
+      // Clear fields
+      nameInput.value = '';
+      emailInput.value = '';
+    }
+  }
